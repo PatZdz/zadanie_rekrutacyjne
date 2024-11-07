1b827cee2b563b01e273146ab072413b4f9be62d
Pat Zdziabek <pat.zdziabek@gmail.com>
2024-11-07 19:55:00 +0100

    Added files to repository

---
+++ android/app/src/main/kotlin/com/example/zadanie_rekrutacyjne/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.zadanie_rekrutacyjne
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:zadanie_rekrutacyjne/screens/gallery_screen.dart' ;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+//Main code that runs the application
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Śląskie',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const GalleryScreen(), 
+    );
+  }
+}
